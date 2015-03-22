a9ecf00eafd332c2802d01511a839b5268a425a4
ChatFawkes <chatfawkes@gmail.com>
2015-03-22 15:07:25 -0400

    Bump version to 1.0.4
    Signed-off-by: ChatFawkes <chatfawkes@gmail.com>

--- src/main/java/com/zerren/interstellarores/reference/Reference.java
+++ src/main/java/com/zerren/interstellarores/reference/Reference.java
@@ -6,7 +6,7 @@ package com.zerren.interstellarores.reference;
 public class Reference {
     public static final String MOD_ID = "InterstellarOres";
     public static final String MOD_NAME = "Interstellar Ores";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
     public static final String CLIENTPROXY_CLASS = "com.zerren.interstellarores.proxy.ClientProxy";
     public static final String SERVERPROXY_CLASS = "com.zerren.interstellarores.proxy.ServerProxy";
     public static final String GUIFACTORY_CLASS = "com.zerren.interstellarores.client.gui.GuiFactory";
